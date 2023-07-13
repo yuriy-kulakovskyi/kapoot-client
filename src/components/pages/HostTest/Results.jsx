d418986f8663a895018555d75214a241fd14c306
yuriy-kulakovskyi <yurakulak42@gmail.com>
2023-07-13 22:19:15 +0300

    detecting if the user lefts the host page the quiz will be deleted from games cloud

--- src/components/pages/HostTest/Results.jsx
+++ src/components/pages/HostTest/Results.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+// useLocation
 import { useLocation } from "react-router-dom";
+
+// onValue
+import { onValue } from "firebase/database";
+
+// ref from firebase
+import { ref } from "firebase/database";
+
+// remove from firebase
+import { remove } from "firebase/database";
+
+// getDatabase from firebase
+import { getDatabase } from "firebase/database";
 
 // styles
 import '../../../styles/HostTest/Results.css';
@@ -8,6 +22,30 @@ const Results = () => {
   const location = useLocation();
   
   const { title, code } = location.state;
+
+  // database
+  const database = getDatabase();
+  const gamesRef = ref(database, "/games");
+
+  // delete game from database by code if the user left the page
+  window.onbeforeunload = () => {
+    // onValue
+    onValue(gamesRef, (snapshot) => {
+      const data = snapshot.val();
+
+      // if data exists
+      if (data) {
+        // foreach loop through data
+        Object.keys(data).forEach((key) => {
+          // if code matches
+          if (data[key].value.code === code) {
+            // remove from database
+            remove(ref(database, `/games/${key}`));
+          }
+        });
+      }
+    });
+  }
 
   return (
     <section className='results'>
